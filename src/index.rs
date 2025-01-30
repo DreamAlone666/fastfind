f5d41d6c17a7bb3be035c09b1ac40e171672c317
DreamAlone <DAlone666@163.com>
2025-01-30 21:14:39 +0800

    Add Index::sync

--- src/index.rs
+++ src/index.rs
@@ -3,25 +3,33 @@ mod find;
 use anyhow::Result;
 use log::debug;
 use std::collections::HashMap;
+use windows::Win32::System::Ioctl::{
+    USN_REASON_CLOSE, USN_REASON_FILE_CREATE, USN_REASON_FILE_DELETE, USN_REASON_RENAME_NEW_NAME,
+};
 
 use crate::ntfs::{UsnRecord, Volume};
 use find::FindIter;
 
+type V = (u64, Box<str>);
+
 pub struct Index {
     driver: String,
-    map: HashMap<u64, (u64, Box<str>)>,
+    map: HashMap<u64, V>,
+    usn: i64,
 }
 
 impl Index {
-    pub fn with_capacity(driver: String, capacity: usize) -> Self {
+    pub fn with_capacity(driver: String, usn: i64, capacity: usize) -> Self {
         Self {
             driver,
             map: HashMap::with_capacity(capacity),
+            usn,
         }
     }
 
     pub fn try_from_volume(vol: &Volume) -> Result<Self> {
-        let mut index = Self::with_capacity(vol.driver().to_string(), 10_0000);
+        let usn = vol.usn_journal_data()?.next_usn;
+        let mut index = Self::with_capacity(vol.driver().to_string(), usn, 10_0000);
         let mut count: u64 = 0;
         for record in vol.file_records::<4096>() {
             index.insert(record?);
@@ -31,9 +39,9 @@ impl Index {
         Ok(index)
     }
 
-    pub fn insert(&mut self, record: UsnRecord) {
+    pub fn insert(&mut self, record: UsnRecord) -> Option<V> {
         self.map
-            .insert(record.frn, (record.parent_frn, record.filename.into()));
+            .insert(record.frn, (record.parent_frn, record.filename.into()))
     }
 
     pub fn driver(&self) -> &str {
@@ -42,5 +50,37 @@ impl Index {
 
     pub fn find_iter<'a>(&'a self, sub: &'a str) -> FindIter<'a> {
         FindIter::new(self, sub)
+    }
+
+    pub fn sync(&mut self, vol: &Volume) -> Result<()> {
+        let id = vol.usn_journal_data()?.id;
+        let mut usn_records = vol.usn_records_from::<4096>(id, self.usn);
+        while let Some(res) = usn_records.next() {
+            let record = res?;
+            // 只匹配文件关闭时的事件
+            match record.reason ^ USN_REASON_CLOSE {
+                USN_REASON_FILE_CREATE => {
+                    debug!("Index({:?})：创建 {:?}", self.driver(), record.filename);
+                    self.insert(record);
+                }
+                USN_REASON_FILE_DELETE => {
+                    self.map.remove(&record.frn);
+                    debug!("Index({:?})：删除 {:?}", self.driver(), record.filename);
+                }
+                USN_REASON_RENAME_NEW_NAME => {
+                    let frn = record.frn;
+                    if let Some((_, old)) = self.insert(record) {
+                        debug!(
+                            "Index({:?})：重命名 {old:?} => {:?}",
+                            self.driver(),
+                            self.map[&frn].1
+                        );
+                    }
+                }
+                _ => {}
+            }
+        }
+        self.usn = usn_records.next_usn();
+        Ok(())
     }
 }
